5d847a54717c03d1ff4c76186951fd68545ef020
Juan David Cruz Garcia <71021752+1KVueltasAlCampo@users.noreply.github.com>
2022-03-24 11:56:23 -0500

    Implemented setStatusClosed

--- main.js
+++ main.js
@@ -56,6 +56,20 @@ function saveIssue(e) {
     }
   }
 
+  function setStatusClosed (id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    
+    for(var i = 0; i < issues.length; i++) {
+      if (issues[i].id == id) {
+        issues[i].status = "Closed";
+      }
+    }
+      
+    localStorage.setItem('issues', JSON.stringify(issues));
+    
+    fetchIssues();
+  }
+
   document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
 
   
